913e673ea1eb9fa2e46ac14bac1257368bca2f6a
Simon Emanuel Schmid <simon@schmid.io>
2023-11-19 02:21:42 +0300

    Integrate Supabase

--- packages/nextjs/pages/api/auth/[...nextauth].ts
+++ packages/nextjs/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import { SupabaseAdapter } from "@auth/supabase-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
@@ -8,8 +9,11 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // ...add more providers here
   ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  }),
 };
 
 export default NextAuth(authOptions);
