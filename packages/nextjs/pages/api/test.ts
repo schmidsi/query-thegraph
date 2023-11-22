191fbbad2d77e29eb430d1a75304dcbc4dcc2b13
Simon Emanuel Schmid <simon@schmid.io>
2023-11-23 00:25:45 +0100

    Download from IPFS with simple fetch

--- packages/nextjs/pages/api/test.ts
+++ packages/nextjs/pages/api/test.ts
@@ -3,26 +3,25 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const DEFAULT_IPFS_URL = "https://api.thegraph.com/ipfs/api/v0" as const;
 
-async function asyncIterableToString(iterable: AsyncIterable<Uint8Array>): Promise<string> {
-  let result = "";
-  const decoder = new TextDecoder();
-
-  for await (const chunk of iterable) {
-    result += decoder.decode(chunk);
-  }
-
-  return result;
-}
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Handle the API request here
   const client = create({ url: DEFAULT_IPFS_URL });
 
-  const data = client.get("QmcUrvLJ4piwBxhUiQEPsdxAkj124cykQ5ZJeE428XoePt");
+  console.log(client);
 
-  const decoded = await asyncIterableToString(data);
+  const response = await fetch(
+    "https://api.thegraph.com/ipfs/api/v0/cat?arg=QmcUrvLJ4piwBxhUiQEPsdxAkj124cykQ5ZJeE428XoePt",
+  );
 
-  console.log(data, decoded);
+  const data = await response.text();
 
-  res.status(200).json({ message: "Hello from the API route!" });
+  console.log(data);
+
+  // const data = client.get("QmcUrvLJ4piwBxhUiQEPsdxAkj124cykQ5ZJeE428XoePt");
+
+  // const decoded = await asyncIterableToString(data);
+
+  // console.log(data, decoded);
+
+  res.status(200).json({ yaml: data });
 }
