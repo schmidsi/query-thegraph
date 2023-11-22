c198c11eefb1e219206da2f905501bfc8bb70d16
Simon Emanuel Schmid <simon@schmid.io>
2023-11-22 17:05:44 +0100

    Create subgraph on deploy

--- README.md
+++ README.md
@@ -10,6 +10,6 @@ Merge these two subgraphs:
 ## Publish a subgraph locally
 
 ```bash
-graph auth http://localhost:3000/api/deploy clp9x4oxo0001lwaq95q9qtmt
+graph auth http://localhost:3000/api/deploy clp9yeu160001m79bxrs8seo4
 graph deploy --node http://localhost:3000/api/deploy subgraph-name
 ```
--- packages/nextjs/pages/api/deploy.ts
+++ packages/nextjs/pages/api/deploy.ts
@@ -1,8 +1,59 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "~~/utils/prisma";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.headers); // Log all request headers
+  /* example headers:
+  {
+    'content-length': '203',
+    'content-type': 'application/json; charset=utf-8',
+    accept: 'application/json',
+    'user-agent': 'jayson-4.0.0',
+    authorization: 'Bearer clp9x4oxo0001lwaq95q9qtmt',
+    host: 'localhost:3000',
+    connection: 'close'
+  }
+  */
   console.log(req.body);
+  /* example body:
+  {
+    method: 'subgraph_deploy',
+    jsonrpc: '2.0',
+    params: {
+      name: 'subgraph-name',
+      ipfs_hash: 'QmcUrvLJ4piwBxhUiQEPsdxAkj124cykQ5ZJeE428XoePt',
+      version_label: '1'
+    },
+    id: '612174a6-6861-4a63-9b8e-b905caa0dd03'
+  }
+  */
+
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    console.log("No token provided");
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const user = await prisma.user.findUnique({ where: { apiKey: token } });
+
+  if (!user) {
+    console.log("Invalid token", token);
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  if (req.body.method !== "subgraph_deploy") {
+    console.log("invalid method");
+    return res.status(400).json({ message: "Invalid method" });
+  }
+
+  const subgraph = await prisma.subgraph.upsert({
+    where: { slug: req.body.params.name },
+    update: {},
+    create: { slug: req.body.params.name, user: { connect: { id: user.id } } },
+  });
+
+  console.log(subgraph);
 
   res.status(200).json({ message: "Request headers logged successfully" });
 }
